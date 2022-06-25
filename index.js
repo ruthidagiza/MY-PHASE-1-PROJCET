5e9bc35fceb3cff4fca3265e14c3f69cd9676d90
IDAGIZA <ruthidagiza@gmail.com>
2022-06-25 13:15:00 +0300

    added a getMovies funtion to fetch the movies form externa API

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //alert("javascript is connected")
+
+let movieslist =[]
+
+
+function getMovies(){
+    console.log("movies request was sent")
+    fetch("https://owen-wilson-wow-api.herokuapp.com/wows/random?results=500")
+    .then(function(response){
+
+console.log("received a response")
+        return response.json();
+    })
+    .then(function(movies){
+        movieslist=movies;
+
+        console.log("an array of json movies received")
+        console.log(movieslist)
+    })
+}
+
+getMovies()
